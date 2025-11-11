d70edf8221c1d33c73a84507c72380290ed2425e
Tinotenda Muzovaka <tinotendamuzovaka5@gmail.com>
2025-11-11 16:33:09 +0000

    Added new pages

--- colts/app/urls.py
+++ colts/app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('table', views.table, name='table'),
+
+    path('matches', views.matches, name='matches'),
+
+    path('about', views.about, name='about'),
 ]
--- colts/app/views.py
+++ colts/app/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index (request):
     return render(request, 'index.html')
+
+def table (request):
+    return render(request, 'table.html')
+
+def matches (request):
+    return render(request, 'matches.html')
+
+def about (request):
+    return render(request, 'about.html')
