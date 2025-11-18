b1b36b672fed3269f27c289ea9fab16dd0fc445e
Tinotenda Muzovaka <tinotendamuzovaka5@gmail.com>
2025-11-18 16:09:32 +0000

    added header image to base file

--- colts/app/views.py
+++ colts/app/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index (request):
-    return render(request, 'indsex.html')
+    return render(request, 'index.html')
 
 def table (request):
     return render(request, 'table.html')
