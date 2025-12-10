0f9be1299c47f39d00c02de5512573ae89fa62a3
Tinotenda Muzovaka <tinotendamuzovaka5@gmail.com>
2025-12-10 09:35:10 +0000

    Updated static file rendering

--- colts/colts/settings.py
+++ colts/colts/settings.py
@@ -139,8 +139,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Media files (User-uploaded content)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/#serving-files-uploaded-by-a-user-during-development
