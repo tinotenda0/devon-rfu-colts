2407c0fe96e5bfe9dbdb908ba835b26fd4f4066c
Tinotenda <tinotendamuzovaka5@gmail.com>
2025-12-16 20:58:37 +0000

    Fixed media file placement

--- colts/colts/settings.py
+++ colts/colts/settings.py
@@ -95,7 +95,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # 🔹 Media files
-MEDIA_URL = "/static/"
-MEDIA_ROOT = BASE_DIR / "static"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
